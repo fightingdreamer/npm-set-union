7eee81939edf5ee820de1151c5af8dcdab412d9a
Patryk Stępień <fighting.dreamer.0@gmail.com>
2023-12-12 00:07:00 +0100

    Add function inline docs

--- lib/set/union.ts
+++ lib/set/union.ts
@@ -1,11 +1,18 @@
 import { reduceWithInitial } from "@fightingdreamer/iter-reduce";
 
+/**
+ * Intersection between two Sets
+ */
 export function union<T>(a: Set<T>, b: Iterable<T>): Set<T> {
   const result = new Set(a);
   for (const elem of b) result.add(elem);
   return result;
 }
 
+/**
+ * Intersection between two Sets
+ */
 export function unionMany<T>(sets: Iterable<Iterable<T>>): Set<T> {
   return reduceWithInitial(sets, union, new Set<T>());
+  return;
 }
